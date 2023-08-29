cbbacc018369896760f4bf5df9680cf6e4a4b134
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-29 23:32:35 +0500

    ✅ join and ArrayWrapper done

--- Day8/index.js
+++ Day8/index.js
@@ -83,3 +83,49 @@ var sortBy = function (arr, fn) {
 };
 
 //-------------------------------------------------------------
+
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+    const result = {};
+    for (let i = 0; i < arr1.length; i++) {
+        result[arr1[i].id] = arr1[i];
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        if (result[arr2[i].id]) {
+            for (const key in arr2[i]) result[arr2[i].id][key] = arr2[i][key];
+        } else {
+            result[arr2[i].id] = arr2[i];
+        }
+    }
+
+    return Object.values(result);
+};
+
+//-------------------------------------------------------------
+
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function (nums) {
+    this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function () {
+    return this.nums.reduce((acc, cur) => acc + cur, 0);
+};
+
+ArrayWrapper.prototype.toString = function () {
+    return `[${this.nums}]`;
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
