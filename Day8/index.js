b44094e767ddd392c10f55f4d41da9c71e947b4c
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-29 22:35:04 +0500

    ✅ 2721. Execute Asynchronous Functions in Parallel

--- Day8/index.js
+++ Day8/index.js
@@ -24,3 +24,30 @@ var debounce = function (fn, t) {
  */
 
 //----------------------------------------------------------------
+
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = async function (functions) {
+    return new Promise((resolve, reject) => {
+        let results = new Array(functions.length);
+        let count = 0;
+        functions.forEach((fn, i) => {
+            fn()
+                .then((value) => {
+                    results[i] = value;
+                    count++;
+                    if (count === functions.length) resolve(results);
+                })
+                .catch((err) => reject(err));
+        });
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
+
+//----------------------------------------------------------------
