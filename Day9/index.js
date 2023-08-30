1e4100cf4f0d0e36fb8bbb504b58fa981accf155
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-30 21:46:36 +0500

    ✅ 2625. Flatten Deeply Nested Array done

--- Day9/index.js
+++ Day9/index.js
@@ -66,3 +66,26 @@ class Calculator {
 }
 
 //----------------------------------------------------------------
+
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+var flat = function (arr, n) {
+    const stack = [...arr.map((item) => [item, n])];
+    const result = [];
+
+    while (stack.length > 0) {
+        const [item, n] = stack.pop();
+
+        if (Array.isArray(item) && n > 0) {
+            stack.push(...item.map((subItem) => [subItem, n - 1]));
+        } else {
+            result.push(item);
+        }
+    }
+    return result.reverse();
+};
+
+//----------------------------------------------------------------
