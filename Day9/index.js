dadb03bff982938c8d10f38ba4a561766b448fcc
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-30 22:03:46 +0500

    ✅ 2705. Compact Object  done

--- Day9/index.js
+++ Day9/index.js
@@ -89,3 +89,23 @@ var flat = function (arr, n) {
 };
 
 //----------------------------------------------------------------
+
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function (obj) {
+    if (obj === null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj !== "object") return obj;
+
+    const compacted = {};
+    for (const key in obj) {
+        let value = compactObject(obj[key]);
+        if (Boolean(value)) compacted[key] = value;
+    }
+
+    return compacted;
+};
+
+//----------------------------------------------------------------
